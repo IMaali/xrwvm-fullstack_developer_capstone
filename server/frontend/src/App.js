52ed4803f4c397ac9bffd63e47ec3d4aae379f41
IMaali <maali5533@gmail.com>
2024-05-20 13:54:09 -0400

    Adding login pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,16 @@
 import LoginPanel from "./components/Login/Login"
+import RegistrationPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      {/* <Route path="/logout" element={<LoginPanel />} /> */}
+      <Route path="/register" element={<RegistrationPanel />} />
+
     </Routes>
+    
   );
 }
 export default App;
